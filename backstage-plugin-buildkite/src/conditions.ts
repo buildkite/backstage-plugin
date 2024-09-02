56a3ca188af17f28eaf043ca1dc98b624f318f07
Ben McNicholl <git@benmcnicholl.com>
2024-09-02 14:50:14 +1000

    Add a helper for checking if metadata set

---
+++ backstage-plugin-buildkite/src/conditions.ts
@@ -0,0 +1,5 @@
+import { Entity } from '@backstage/catalog-model';
+
+export const isBuildkiteAvailable = (entity: Entity) => {
+  return Boolean(entity.metadata.annotations?.['buildkite.com/project-slug']);
+};

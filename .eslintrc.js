dcdff8d6da6018ae421f9e91fd56c3c066965aea
Ben McNicholl <git@benmcnicholl.com>
2024-11-13 13:17:14 +1100

    configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ import pluginReact from "eslint-plugin-react";
 
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [
+module.exports = [
   {
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]
   },
